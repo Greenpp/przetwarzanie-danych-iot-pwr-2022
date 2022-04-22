6b100b4fe7b6fc47e01a2a8189d7a077b63fcc7d
Jakub Brzoza <63004576+Qbsoonn@users.noreply.github.com>
2022-04-23 01:52:58 +0200

    python file

---
+++ telegraf/get_data.py
@@ -0,0 +1,37 @@
+import paho.mqtt.subscribe as subscribe
+import datetime
+import json
+import fire
+
+def get_data_points(device_name):
+    m = subscribe.simple(
+        topics=['#'],
+        hostname="eu1.cloud.thethings.network",
+        port=1883,
+        auth={'username':"pwr-iot-2022",'password':'NNSXS.24TRHAQZJCF3UL74GMUXXVLR63KLYLKE2SEXSNI.O2GZUKUNG3I5DJSPGBKE7KCD45KSWQC45AGEVJ2DGCNJB3ZFYKWQ'},
+        msg_count=2
+        )
+    if device_name == 'paw': #chuj wi ktory to ktory
+        device = m[0]
+    else:
+        device = m[1]
+        
+    payload = json.loads(device.payload)['uplink_message']['decoded_payload']
+    humidity = payload['humidity']
+    temperature = payload['temperature']
+    timestamp = datetime.datetime.utcnow().isoformat()
+
+    datapoints = [
+        {
+            'device_name': device_name,
+            'time': timestamp,
+            'fields':{
+                'humidity': humidity,
+                'temperature': temperature,
+            }
+        }
+    ]
+    return datapoints
+
+if __name__ == '__main__':
+    fire.Fire(get_data_points)
